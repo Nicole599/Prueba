20bdb6187eb95605460214314828a26d66679a63
Dayana Lalangui <133123640+Nicole599@users.noreply.github.com>
2024-06-07 10:14:30 -0500

    Delete Producto_EjercicioPrueba directory

--- Producto_EjercicioPrueba/src/main/java/usuario/proyecto/prueba/dao/IProductoDao.java
+++
@@ -1,8 +0,0 @@
-package usuario.proyecto.prueba.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import usuario.proyecto.prueba.entity.Producto;
-
-public interface IProductoDao extends JpaRepository<Producto,Long>{	
-}
